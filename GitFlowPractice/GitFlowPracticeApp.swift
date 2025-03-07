02e7c77ebfe4f92f6a96c4554a3159d59b62e0ee
Vladislav Todorov <it.vtodorov@gmail.com>
2025-03-07 13:36:14 +0300

    Initial Commit

---
+++ GitFlowPractice/GitFlowPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowPracticeApp.swift
+//  GitFlowPractice
+//
+//  Created by Fenominall on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GitFlowPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
